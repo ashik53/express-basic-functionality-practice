0411be92976fd18ee809855e8a6d6ca62385d1c1
ashik53 <ashikiqbal53@gmail.com>
2020-09-10 22:38:37 +0600

    render a page after post route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+var bodyParser = require('body-parser');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/insert', function(req, res, next){
+  var item = {
+    name: req.body.name,
+    age: req.body.age
+  }
+  res.render('form-data', {item: item});
+})
 
 router.get('/list', function(req, res, next){
   res.render('all_infos', {title: 'helo'});
